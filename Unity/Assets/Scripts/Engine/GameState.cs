7e5a3252e3a178e66f46de263f59de6163a1310c
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2016-10-25 02:00:45 +0200

    Implementation du a-star en cours...

---
+++ Unity/Assets/Scripts/Engine/GameState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public struct GameState
+{
+
+    public Vehicle player;
+    public Vehicle AI;
+    public GameObject[] bananaPool;
+    public float elapsedTime;
+}
---
+++ Unity/Assets/Scripts/Engine/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    /**
+        Structure de la piste
+    **/
+    private Tile[] trackTiles;
+    private int trackWidth;
+    private int trackHeight;
+
+    /**
+        Nombre de tours effectués par les joueurs
+        et nombre de tours total
+    **/
+    private int trackLapsCount;
+    private int playerLapsCount;
+    private int AILapsCount;
+
+    private int maxBananasCount;
+
+    public Tile getTiles(int x, int y)
+    {
+        return trackTiles[y * trackWidth + x];
+    }
+
+
+
+
+}
