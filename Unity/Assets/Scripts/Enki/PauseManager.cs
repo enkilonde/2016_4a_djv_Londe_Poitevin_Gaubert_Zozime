b8810f990f30abc874e261342dd9d618ed051344
Thibaut <joethesardine@gmail.com>
2016-10-25 22:07:57 +0200

    Et rajouté la touche "p" pour pause en plus de "esc".

--- Unity/Assets/Scripts/Enki/PauseManager.cs
+++ Unity/Assets/Scripts/Enki/PauseManager.cs
@@ -16,7 +16,7 @@ public class PauseManager : MonoBehaviour
 	void Update ()
     {
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
             TriggerPause();
         }
