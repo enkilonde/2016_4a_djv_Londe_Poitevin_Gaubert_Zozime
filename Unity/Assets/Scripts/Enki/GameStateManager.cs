28f0d10c702f59d3fa7f56b0d088ba23782398e1
Enki <enkilonde@gmail.com>
2016-10-25 16:46:36 +0200

    fix Ajout des point meta

--- Unity/Assets/Scripts/Enki/GameStateManager.cs
+++ Unity/Assets/Scripts/Enki/GameStateManager.cs
@@ -7,6 +7,9 @@ public class GameStateManager : MonoBehaviour
     public GameState gameState;
     public Transform player;
 
+    public float rotationSpeed = 90;
+    public float maxSpeed = 15;
+
     void Awake()
     {
 
@@ -15,12 +18,27 @@ public class GameStateManager : MonoBehaviour
 
     }
 
-    void CustomUptade()
+    void InitGameState()
     {
-        
+        gameState.player.rotationSpeed = rotationSpeed;
+        gameState.player.maxSpeed = maxSpeed;
+
+        UpdateGameState();
+    }
+
+    void UpdateGameState()
+    {
+        gameState.player.position = player.position;
+        gameState.player.orientation = player.rotation.eulerAngles.y;
 
 
     }
+
+    void ApplyNextState()
+    {
+
+    }
+
 
 
     void Forsee()
